25319f536ef458fa46c8ec913d4d381380d31c74
Marat Abdullin <dakota@brokenpipe.ru>
2013-05-11 23:22:35 +0400

    Handling nodeset values for xml2json mapper.

--- libxrlt/xml2json.cc
+++ libxrlt/xml2json.cc
@@ -23,22 +23,23 @@ v8::Persistent<v8::ObjectTemplate> xrltXML2JSONTemplate;
 // C++ structure to be associated with XML2JSON cache.
 struct xrltXML2JSONCache {
     xrltXML2JSONCache(v8::Handle<v8::Array> data) : index(0), data(data) {};
-    bool get(xmlNodePtr node, v8::Handle<v8::Value> *value);
-    void set(xmlNodePtr node, v8::Handle<v8::Value> value);
+    bool get(void *key, v8::Handle<v8::Value> *value);
+    void set(void *key, v8::Handle<v8::Value> value);
 
   private:
-    uint32_t                         index;
-    std::map<xmlNodePtr, uint32_t>   cache;
-    v8::Handle<v8::Array>            data;
+    uint32_t                     index;
+    std::map<void *, uint32_t>   cache;
+    v8::Handle<v8::Array>        data;
 };
 
 
 bool
-xrltXML2JSONCache::get(xmlNodePtr node, v8::Handle<v8::Value> *value)
+xrltXML2JSONCache::get(void *key, v8::Handle<v8::Value> *value)
 {
-    std::map<xmlNodePtr, uint32_t>::iterator i;
+    std::map<void *, uint32_t>::iterator i;
 
-    i = cache.find(node);
+    i = cache.find(key);
+
     if (i != cache.end()) {
         *value = data->Get(i->second);
         return true;
@@ -49,9 +50,10 @@ xrltXML2JSONCache::get(xmlNodePtr node, v8::Handle<v8::Value> *value)
 
 
 void
-xrltXML2JSONCache::set(xmlNodePtr node, v8::Handle<v8::Value> value)
+xrltXML2JSONCache::set(void *key, v8::Handle<v8::Value> value)
 {
-    cache.insert(std::pair<xmlNodePtr, uint32_t>(node, index));
+    cache.insert(std::pair<void *, uint32_t>(key, index));
+
     data->Set(index++, value);
 }
 
@@ -115,6 +117,7 @@ struct xrltXML2JSONTypeMap {
         i = types.find((const char *)str);
         return i != types.end() ? i->second : XRLT_JSON2XML_UNKNOWN;
     }
+
   private:
     std::map<std::string, xrltJSON2XMLType> types;
 };
@@ -123,7 +126,8 @@ xrltXML2JSONTypeMap XRLT_TYPE_MAP;
 
 // C++ structure to be associated with XML2JSON JavaScript object.
 struct xrltXML2JSONData {
-    xrltXML2JSONData(xmlNodePtr parent, xrltXML2JSONCache *cache);
+    xrltXML2JSONData(xmlNodePtr parent, xmlXPathObjectPtr val,
+                     xrltXML2JSONCache *cache);
 
     xrltJSON2XMLType                         type;
     std::vector<xmlNodePtr>                  nodes;
@@ -132,21 +136,33 @@ struct xrltXML2JSONData {
 };
 
 
-xrltXML2JSONData::xrltXML2JSONData(xmlNodePtr parent,
+xrltXML2JSONData::xrltXML2JSONData(xmlNodePtr parent, xmlXPathObjectPtr val,
                                    xrltXML2JSONCache *cache) : cache(cache)
 {
-    xmlNodePtr n = parent != NULL ? parent->children : NULL;
+    xmlNodePtr   n;
 
-    // Create a list of nodes for XML2JSON object.
-    while (n != NULL) {
-        if (!xmlIsBlankNode(n)) {
-            nodes.push_back(n);
+    if (parent != NULL) {
+        n = parent->children;
+
+        // Create a list of nodes for XML2JSON object.
+        while (n != NULL) {
+            if (!xmlIsBlankNode(n)) {
+                nodes.push_back(n);
+            }
+            n = n->next;
         }
-        n = n->next;
+    } else if (val != NULL && val->nodesetval != NULL &&
+               val->nodesetval->nodeTab != NULL)
+    {
+        int   i;
+
+        for (i = 0; i < val->nodesetval->nodeNr; i++) {
+            nodes.push_back(val->nodesetval->nodeTab[i]);
+        }
     }
 
-    std::vector<xmlNodePtr>::iterator i;
-    std::string key;
+    std::vector<xmlNodePtr>::iterator   i;
+    std::string                         key;
 
     // Create a multimap to find nodes by names.
     for(i = nodes.begin(); i != nodes.end(); i++) {
@@ -158,10 +174,11 @@ xrltXML2JSONData::xrltXML2JSONData(xmlNodePtr parent,
     }
 
     // Determine XML2JSON object type.
-    xmlChar * typestr = NULL;
+    xmlChar  *typestr = NULL;
 
     if (parent != NULL) {
         typestr = xmlGetNsProp(parent, XRLT_JSON2XML_ATTR_TYPE, XRLT_NS);
+
         if (typestr != NULL) {
             type = XRLT_TYPE_MAP.getType(typestr);
             xmlFree(typestr);
@@ -189,15 +206,20 @@ xrltXML2JSONWeakCallback(v8::Persistent<v8::Value> obj, void *payload)
 
 // XML2JSON JavaScript object creator. Pretty much to optimize here.
 v8::Handle<v8::Value>
-xrltXML2JSONCreateInternal(xmlNodePtr parent, xrltXML2JSONCache *cache)
+xrltXML2JSONCreateInternal(xmlNodePtr parent, xmlXPathObjectPtr val,
+                           xrltXML2JSONCache *cache)
 {
     v8::Handle<v8::Value>   ret;
 
-    if (cache != NULL && cache->get(parent, &ret)) {
-        return ret;
+    if (cache != NULL) {
+        if ((parent != NULL && cache->get(parent, &ret)) ||
+            (val != NULL && cache->get(val, &ret)))
+        {
+            return ret;
+        }
     }
 
-    xrltXML2JSONData       *data = new xrltXML2JSONData(parent, cache);
+    xrltXML2JSONData       *data = new xrltXML2JSONData(parent, val, cache);
     bool                    isXML2JSON = false;
 
     if (data->type == XRLT_JSON2XML_ARRAY) {
@@ -206,7 +228,8 @@ xrltXML2JSONCreateInternal(xmlNodePtr parent, xrltXML2JSONCache *cache)
         uint32_t               i;
 
         for (i = 0; i < data->nodes.size(); i++) {
-            arr->Set(i, xrltXML2JSONCreateInternal(data->nodes[i], cache));
+            arr->Set(i,
+                     xrltXML2JSONCreateInternal(data->nodes[i], NULL, cache));
         }
         ret = arr;
     } else {
@@ -278,15 +301,20 @@ xrltXML2JSONCreate(xmlXPathObjectPtr value)
         case XPATH_STRING:
             return v8::String::New((const char *)value->stringval);
         case XPATH_NODESET:
-            return v8::Undefined();
-        case XPATH_USERS:
             break;
         case XPATH_UNDEFINED:
         case XPATH_POINT:
         case XPATH_RANGE:
         case XPATH_LOCATIONSET:
+        case XPATH_USERS:
         case XPATH_XSLT_TREE:
             return v8::Undefined();
+    }
+
+    if (value->nodesetval == NULL || value->nodesetval->nodeTab == NULL ||
+        value->nodesetval->nodeNr == 0)
+    {
+        return v8::Undefined();
     }
 
     // We will store objects in JavaScript array avoid freeing them by GC
@@ -305,7 +333,14 @@ xrltXML2JSONCreate(xmlXPathObjectPtr value)
     cacheobj.MakeWeak(cache, xrltXML2JSONCacheWeakCallback);
     cacheobj->SetInternalField(0, v8::External::New(cache));
 
-    return xrltXML2JSONCreateInternal((xmlNodePtr)value->user, cache);
+    if (value->nodesetval->nodeNr == 1 &&
+        value->nodesetval->nodeTab[0]->type == XML_DOCUMENT_NODE)
+    {
+        return xrltXML2JSONCreateInternal(value->nodesetval->nodeTab[0], NULL,
+                                          cache);
+    } else {
+        return xrltXML2JSONCreateInternal(NULL, value, cache);
+    }
 }
 
 
@@ -330,14 +365,14 @@ xrltXML2JSONGetProperty(v8::Local<v8::String> name,
         }
 
         if (values.size() == 1) {
-            return xrltXML2JSONCreateInternal(values[0], data->cache);
+            return xrltXML2JSONCreateInternal(values[0], NULL, data->cache);
         } else if (values.size() > 1) {
             uint32_t               index = 0;
             v8::Local<v8::Array>   ret = v8::Array::New();
 
             for (index = 0; index < values.size(); index++) {
                 ret->Set(index,
-                         xrltXML2JSONCreateInternal(values[index],
+                         xrltXML2JSONCreateInternal(values[index], NULL,
                                                     data->cache));
             }
             return ret;
